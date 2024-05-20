a062022778c2cf5ee3d8027514f2626a6a8fc951
kavishkajanith29 <kavishkajanith2000@gmail.com>
2024-05-20 21:55:21 +0530

    add teacher registration

--- ApexAcademy/ApexAcademy/src/components/students/studentForm/studentsForm.jsx
+++ ApexAcademy/ApexAcademy/src/components/students/studentForm/studentsForm.jsx
@@ -1,18 +1,27 @@
 import { Button, Form } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import axios from 'axios';
 import './form.css'
 import { useState } from 'react';
 
 function StudentForm() {
-  const [workingOnOtherInstitute, setWorkingOnOtherInstitute] = useState(null);
   const [additionalFields, setAdditionalFields] = useState([]);
-
-  const handleWorkingOnOtherInstituteChange = (event) => {
-    setWorkingOnOtherInstitute(event.target.value);
-    // Reset additional fields when changing the selection
-    setAdditionalFields([]);
-  };
+  const [formData, setFormData] = useState({
+    studentname: '',
+    address: '',
+    school: '',
+    dob: '',
+    parentsname: '',
+    parentsoccupation: '',
+    email: '',
+    phonenumber: '',
+    medium: '',
+    grade: '',
+    registrationdate: new Date().toLocaleString("sv-SE", { timeZone: "Asia/Colombo", hour12: false }).slice(0, 16),
+    paymentReceipt: null,
+  });
+  const [gradeOptions, setGradeOptions] = useState([]);
+  const currentYear = new Date().getFullYear();
 
   const handleAddField = () => {
     setAdditionalFields([...additionalFields, ""]);
@@ -24,114 +33,183 @@ function StudentForm() {
     setAdditionalFields(newFields);
   };
 
+ 
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({
+      ...formData,
+      [id]: value
+    });
+
+    if (id === "exam") {
+      if (value === "OL") {
+        setGradeOptions(['Grade 06', 'Grade 07', 'Grade 08', 'Grade 09', 'Grade 10', 'Grade 11']);
+      } else if (value === "AL") {
+        setGradeOptions(['Grade 12', 'Grade 13']);
+      }
+    }
+  };
+
+  const generateExamYearOptions = () => {
+    const options = [];
+    for (let i = 0; i < 7; i++) {
+      const year = currentYear + i;
+      options.push(<option key={year} value={year}>{year}</option>);
+    }
+    return options;
+  };
+
+  const getExamYearLabel = () => {
+    if (formData.exam === "OL") {
+      return "Exam Year of OL";
+    } else if (formData.exam === "AL") {
+      return "Exam Year of AL";
+    }
+    return "Exam Year";
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const studentid = `${formData.exam}${formData.examYear}`;
+
+    try {
+      const dataToSend = {
+        studentid: studentid,
+        studentname: formData.studentname,
+        address: formData.address,
+        school: formData.school,
+        dob: formData.dob,
+        parentsname: formData.parentsname,
+        parentsoccupation: formData.parentsoccupation,
+        email: formData.email,
+        phonenumber: formData.phonenumber,
+        medium: formData.medium,
+        grade: formData.grade,
+        registrationdate: formData.registrationdate,
+        password: formData.email
+      };
+
+      const response = await axios.post('http://localhost:8085/api/v1/student/add', dataToSend);
+      console.log(response.data);
+      resetForm();
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
+  };
+
+  const resetForm = () => {
+    setFormData({
+      studentname: '',
+      address: '',
+      school: '',
+      dob: '',
+      parentsname: '',
+      parentsoccupation: '',
+      email: '',
+      phonenumber: '',
+      medium: '',
+      exam: '',
+      grade: '',
+      registrationdate: new Date().toLocaleString("sv-SE", { timeZone: "Asia/Colombo", hour12: false }).slice(0, 16),
+      examYear: '',
+    });
+    setGradeOptions([]);
+  };
+
+
+  
+
   return (
-    <Form>
+    <div className="clssregister">
+      <Form onSubmit={handleSubmit}>
       <fieldset>
         <Form.Group className="mb-3">
-          <Form.Label htmlFor="disabledTextInput" className="form-label">Student Name</Form.Label>
-          <Form.Control id="disabledTextInput" placeholder="Enter Student Name" />
+          <Form.Label htmlFor="studentname">Student Name</Form.Label>
+          <Form.Control id="studentname" placeholder="Enter Student Name" onChange={handleChange} value={formData.studentname}/>
         </Form.Group>
 
         <Form.Group className="mb-3">
-          <Form.Label htmlFor="disabledTextInput" className="form-label">Address</Form.Label>
-          <Form.Control id="disabledTextInput" placeholder="Enter Student Address" />
+          <Form.Label htmlFor="address">Address</Form.Label>
+          <Form.Control id="address" placeholder="Enter Student Address" onChange={handleChange} value={formData.address}/>
         </Form.Group>
 
         <Form.Group className="mb-3">
-          <Form.Label htmlFor="disabledTextInput" className="form-label">School</Form.Label>
-          <Form.Control id="disabledTextInput" placeholder="Enter School Name" />
+          <Form.Label htmlFor="school">School</Form.Label>
+          <Form.Control id="school" placeholder="Enter School Name" onChange={handleChange} value={formData.school}/>
         </Form.Group>
 
         <Form.Group className="mb-3">
-          <Form.Label htmlFor="dobInput" className="form-label">Date of Birth</Form.Label>
-          <Form.Control id="dobInput"type="date"/>
+          <Form.Label htmlFor="dob">Date of Birth</Form.Label>
+          <Form.Control id="dob"type="date" onChange={handleChange} value={formData.dob}/>
         </Form.Group>
 
         <Form.Group className="mb-3">
-          <Form.Label htmlFor="disabledTextInput" className="form-label">Parents/ Legal Guardian Name</Form.Label>
-          <Form.Control id="disabledTextInput" placeholder="Enter Name" />
+          <Form.Label htmlFor="parentsname">Parents/ Legal Guardian Name</Form.Label>
+          <Form.Control id="parentsname" placeholder="Enter Name" onChange={handleChange} value={formData.parentsname}/>
         </Form.Group>
 
         <Form.Group className="mb-3">
-          <Form.Label htmlFor="disabledTextInput" className="form-label">Parents/ Legal Guardian Occupation</Form.Label>
-          <Form.Control id="disabledTextInput" placeholder="Enter Occupation" />
+          <Form.Label htmlFor="parentsoccupation">Parents/ Legal Guardian Occupation</Form.Label>
+          <Form.Control id="parentsoccupation" placeholder="Enter Occupation" onChange={handleChange} value={formData.parentsoccupation}/>
         </Form.Group>
 
         <Form.Group className="mb-3">
-          <Form.Label htmlFor="emailInput" className="form-label">Email Address</Form.Label>
-          <Form.Control id="emailInput"type="email"placeholder="Enter Email Address"/>
+          <Form.Label htmlFor="email">Email Address</Form.Label>
+          <Form.Control id="email"type="email"placeholder="Enter Email Address" onChange={handleChange} value={formData.email} />
         </Form.Group>
 
         <Form.Group className="mb-3">
-          <Form.Label htmlFor="phoneNumberInput" className="form-label">Phone Number</Form.Label>
-          <Form.Control id="phoneNumberInput" type="tel" placeholder="Enter Phone Number"/>
+          <Form.Label htmlFor="phonenumber">Phone Number</Form.Label>
+          <Form.Control id="phonenumber" type="tel" placeholder="Enter Phone Number" onChange={handleChange} value={formData.phonenumber}/>
         </Form.Group>
 
         <Form.Group className="mb-3">
-          <Form.Label htmlFor="disabledSelect" className="form-label">Medium</Form.Label>
-          <Form.Select aria-label="Default select example">
-            <option>Select the medium</option>
-            <option value="sinhala">Sinhala</option>
-            <option value="english">English</option>
-          </Form.Select>
-        </Form.Group>
+            <Form.Label htmlFor="medium">Medium</Form.Label>
+            <Form.Select id="medium" aria-label="Medium" onChange={handleChange} value={formData.medium}>
+              <option>Select the medium</option>
+              <option value="Sinhala">Sinhala</option>
+              <option value="English">English</option>
+            </Form.Select>
+          </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label htmlFor="disabledSelect" className="form-label">Select Grade</Form.Label>
-          <Form.Select aria-label="Default select example">
-            <option>Select the Grade</option>
-            <option value="6">Grade 06</option>
-            <option value="6">Grade 07</option>
-            <option value="6">Grade 08</option>
-            <option value="6">Grade 09</option>
-            <option value="6">Grade 10</option>
-            <option value="6">Grade 11</option>
-          </Form.Select>
-        </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label htmlFor="disabledSelect" className="form-label">Select Subjects</Form.Label>
-          <div className="checkbox-container">
-            <Form.Check type="checkbox" label="Maths" />
-            <Form.Check type="checkbox" label="Science" />
-            <Form.Check type="checkbox" label="English" />
-            <Form.Check type="checkbox" label="History" />
-            <Form.Check type="checkbox" label="Sinhala" />
-            <Form.Check type="checkbox" label="ICT" />
-            <Form.Check type="checkbox" label="English Lit." />
-            <Form.Check type="checkbox" label="Commerce" />
-            <Form.Check type="checkbox" label="Tamil" />
-            <Form.Check type="checkbox" label="Art" />
-            <Form.Check type="checkbox" label="Health & Physical Edu." />
-            <Form.Check type="checkbox" label="Geography" />
-            <Button onClick={handleAddField}>Other...</Button>
-            </div>
-              {additionalFields.map((field, index) => (
-                <div key={index}>
-                  <Form.Control
-                    type="text"
-                    placeholder={`Other Subject   ${index + 1}`}
-                    value={field}
-                    onChange={(e) => handleAdditionalFieldChange(index, e.target.value)}
-                  />
-                </div>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="exam">Exam</Form.Label>
+            <Form.Select id="exam" aria-label="exam" onChange={handleChange} value={formData.exam}>
+              <option>Select the Exam</option>
+              <option value="OL">O/L</option>
+              <option value="AL">A/L</option>
+            </Form.Select>
+          </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label htmlFor="examYear">{getExamYearLabel()}</Form.Label>
+              <Form.Select id="examYear" aria-label="examYear" onChange={handleChange} value={formData.examYear}>
+                {generateExamYearOptions()}
+              </Form.Select>
+            </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="grade">Select Grade</Form.Label>
+            <Form.Select id="grade" aria-label="grade" onChange={handleChange} value={formData.grade}>
+              <option>Select the Grade</option>
+              {gradeOptions.map((grade, index) => (
+                <option key={index} value={grade}>{grade}</option>
               ))}
-            
-
-        </Form.Group>
-
-
-
-        
+            </Form.Select>
+          </Form.Group>
 
         <Form.Group className="mb-3">
-          <Form.Label htmlFor="dobInput" className="form-label">Registration Date</Form.Label>
-          <Form.Control id="dobInput"type="date"/>
-        </Form.Group>
+            <Form.Label htmlFor="registrationdate">Registration Date</Form.Label>
+            <Form.Control id="registrationdate" type="datetime-local" value={formData.registrationdate} readOnly />
+          </Form.Group>
 
         <Button type="submit">Register</Button>
       </fieldset>
     </Form>
+    </div>
   );
 }
 
--- ApexAcademy/ApexAcademy/src/components/teachers/teachersForm/teachersForm.jsx
+++ ApexAcademy/ApexAcademy/src/components/teachers/teachersForm/teachersForm.jsx
@@ -1,113 +1,271 @@
-import { Button, Form } from 'react-bootstrap';
+import { Button, Form, FormControl, FormLabel } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-import './form.css'
+import axios from 'axios';
+import './form.css';
 import { useState } from 'react';
+import Swal from 'sweetalert2';
+import { useNavigate} from "react-router-dom";
+
 
 function TeachersForm() {
-  const [workingOnOtherInstitute, setWorkingOnOtherInstitute] = useState(null);
-  const [additionalFields, setAdditionalFields] = useState([]);
-
-  const handleWorkingOnOtherInstituteChange = (event) => {
-    setWorkingOnOtherInstitute(event.target.value);
-    // Reset additional fields when changing the selection
-    setAdditionalFields([]);
+  const initialFormData = {
+    teacherid: 'TCH',
+    teachername: '',
+    address: '',
+    subject: '',
+    email: '',
+    phoneNumbers: [''],
+    dob: '',
+    grade: [],
+    workingPlaces: [],
+    medium: '',
+    registrationdate: new Date().toLocaleString('sv-SE', { timeZone: 'Asia/Colombo', hour12: false }).slice(0, 16),
   };
 
-  const handleAddField = () => {
-    setAdditionalFields([...additionalFields, ""]);
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState(initialFormData);
+
+  const handleAddField = (field) => {
+    setFormData({
+      ...formData,
+      [field]: [...formData[field], ''],
+    });
   };
 
-  const handleAdditionalFieldChange = (index, value) => {
-    const newFields = [...additionalFields];
-    newFields[index] = value;
-    setAdditionalFields(newFields);
+  const handleDeleteField = (field, index) => {
+    const updatedFields = formData[field].filter((_, idx) => idx !== index);
+    setFormData({
+      ...formData,
+      [field]: updatedFields,
+    });
+  };
+
+  const handleFieldChange = (field, index, value) => {
+    const updatedFields = [...formData[field]];
+    updatedFields[index] = value;
+    setFormData({
+      ...formData,
+      [field]: updatedFields,
+    });
+  };
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({
+      ...formData,
+      [id]: value,
+    });
+  };
+
+  const handleCheckboxChange = (e) => {
+    const { value, checked } = e.target;
+    let updatedGrades = [...formData.grade];
+    if (checked) {
+      updatedGrades.push(value);
+    } else {
+      updatedGrades = updatedGrades.filter((grade) => grade !== value);
+    }
+    setFormData({
+      ...formData,
+      grade: updatedGrades,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Validate form fields
+    if (!validateForm()) {
+      return;
+    }
+
+    try {
+      const dataToSend = {
+        ...formData,
+        password: formData.email,
+      };
+
+      const response = await axios.post('http://localhost:8085/api/v1/teacher/add', dataToSend);
+
+      // Show success message
+      Swal.fire({
+        icon: 'success',
+        title: 'Registration Successful!',
+        text: 'Teacher has been registered successfully.',
+      });
+      navigate(`/teachers`);
+
+      resetForm();
+    } catch (error) {
+      console.error('Error submitting form:', error);
+      // Show error message
+      Swal.fire({
+        icon: 'error',
+        title: 'Registration Failed',
+        text: 'Failed to register teacher. Please try again later.',
+      });
+    }
+  };
+
+  const resetForm = () => {
+    setFormData(initialFormData);
+  };
+
+  const validateForm = () => {
+    if (
+      formData.teachername &&
+      formData.address &&
+      formData.subject &&
+      formData.email &&
+      formData.phoneNumbers.length > 0
+    ) {
+      return true;
+    } else {
+      // Show validation error message
+      Swal.fire({
+        icon: 'error',
+        title: 'Validation Error',
+        text: 'Please fill in all required fields and provide at least one phone number.',
+      });
+      return false;
+    }
   };
 
   return (
-    <Form>
-      <fieldset>
-        <Form.Group className="mb-3">
-          <Form.Label htmlFor="disabledTextInput">Name</Form.Label>
-          <Form.Control id="disabledTextInput" placeholder="Enter Name" />
-        </Form.Group>
+    <div className="teacherregister">
+      <Form onSubmit={handleSubmit}>
+        <fieldset>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="teachername">Teacher Name *</Form.Label>
+            <Form.Control
+              id="teachername"
+              placeholder="Enter Teacher Name"
+              onChange={handleChange}
+              value={formData.teachername}
+              required
+            />
+          </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label htmlFor="disabledTextInput">Address</Form.Label>
-          <Form.Control id="disabledTextInput" placeholder="Enter Address" />
-        </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="address">Address *</Form.Label>
+            <Form.Control
+              id="address"
+              placeholder="Enter Address"
+              onChange={handleChange}
+              value={formData.address}
+              required
+            />
+          </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label htmlFor="emailInput">Email Address</Form.Label>
-          <Form.Control id="emailInput"type="email"placeholder="Enter Email Address"/>
-        </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="subject">Subject *</Form.Label>
+            <Form.Control
+              id="subject"
+              placeholder="Enter Subject"
+              onChange={handleChange}
+              value={formData.subject}
+              required
+            />
+          </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label htmlFor="phoneNumberInput">Phone Number</Form.Label>
-          <Form.Control id="phoneNumberInput" type="tel" placeholder="Enter Phone Number"/>
-        </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="email">Email Address *</Form.Label>
+            <Form.Control
+              id="email"
+              type="email"
+              placeholder="Enter Email Address"
+              onChange={handleChange}
+              value={formData.email}
+              required
+            />
+          </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label htmlFor="dobInput">Date of Birth</Form.Label>
-          <Form.Control id="dobInput"type="date"/>
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label htmlFor="disabledSelect">Select Grade</Form.Label>
-          <div className="checkbox-container">
-            <Form.Check type="checkbox" label="Grade 06" value="6"/>
-            <Form.Check type="checkbox" label="Grade 07" value="7"/>
-            <Form.Check type="checkbox" label="Grade 08" value="8"/>
-            <Form.Check type="checkbox" label="Grade 09" value="9"/>
-            <Form.Check type="checkbox" label="Grade 10" value="10"/>
-            <Form.Check type="checkbox" label="Grade 11" value="11"/>
-            </div>
-        </Form.Group>
-
-        <Form.Group className="mb-3">
-          <Form.Label htmlFor="disabledSelect">Currently working on other Institute</Form.Label>
-          <Form.Select aria-label="Default select example" onChange={handleWorkingOnOtherInstituteChange}>
-            <option>Select the Yes/No</option>
-            <option value="Yes">Yes</option>
-            <option value="No">No</option>
-          </Form.Select>
-        </Form.Group>
-
-        {workingOnOtherInstitute === "Yes" && (
-          <div>
-            <Form.Group className="mb-3">
-              {additionalFields.map((field, index) => (
-                <div key={index}>
-                  <Form.Control
-                    type="text"
-                    placeholder={`Institute  ${index + 1}`}
-                    value={field}
-                    onChange={(e) => handleAdditionalFieldChange(index, e.target.value)}
-                  />
-                </div>
-              ))}
-              <Button onClick={handleAddField}>Add Institute</Button>
+          {formData.phoneNumbers.map((phone, index) => (
+            <Form.Group className="mb-3" key={index}>
+              <Form.Label htmlFor={`phoneNumber${index}`}>Phone Number {index + 1} *</Form.Label>
+              <div className="d-flex">
+                <Form.Control
+                  id={`phoneNumber${index}`}
+                  type="tel"
+                  placeholder="Enter Phone Number"
+                  value={phone}
+                  onChange={(e) => handleFieldChange('phoneNumbers', index, e.target.value)}
+                  required
+                />
+                {index > 0 && (
+                  <Button variant="danger" onClick={() => handleDeleteField('phoneNumbers', index)}>
+                    Delete
+                  </Button>
+                )}
+              </div>
             </Form.Group>
-          </div>
-        )}
+          ))}
+          <Button onClick={() => handleAddField('phoneNumbers')}>Add Phone Number</Button>
 
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="dob">Date of Birth</Form.Label>
+            <Form.Control id="dob" type="date" onChange={handleChange} value={formData.dob} />
+          </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label htmlFor="disabledSelect">Medium</Form.Label>
-          <Form.Select aria-label="Default select example">
-            <option>Select the medium</option>
-            <option value="sinhala">Sinhala</option>
-            <option value="english">English</option>
-          </Form.Select>
-        </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="medium">Medium *</Form.Label>
+            <Form.Select id="medium" aria-label="Medium" onChange={handleChange} value={formData.medium} required>
+              <option>Select the medium</option>
+              <option value="Sinhala">Sinhala</option>
+              <option value="English">English</option>
+            </Form.Select>
+          </Form.Group>
 
-        <Form.Group className="mb-3">
-          <Form.Label htmlFor="dobInput">Institute Registration Date</Form.Label>
-          <Form.Control id="dobInput"type="date"/>
-        </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Grade *</Form.Label>
+            <div className="checkbox-container">
+              <Form.Check
+                type="checkbox"
+                label="O/L"
+                value="O/L"
+                checked={formData.grade.includes('O/L')}
+                onChange={handleCheckboxChange}
+              />
+              <Form.Check
+                type="checkbox"
+                label="A/L"
+                value="A/L"
+                checked={formData.grade.includes('A/L')}
+                onChange={handleCheckboxChange}
+              />
+            </div>
+          </Form.Group>
 
-        <Button type="submit">Register</Button>
-      </fieldset>
-    </Form>
+          {formData.workingPlaces.map((place, index) => (
+            <Form.Group className="mb-3" key={index}>
+              <Form.Label htmlFor={`workingPlace${index}`}>Working Place {index + 1}</Form.Label>
+              <div className="d-flex">
+                <Form.Control
+                  id={`workingPlace${index}`}
+                  placeholder="Enter Working Place"
+                  value={place}
+                  onChange={(e) => handleFieldChange('workingPlaces', index, e.target.value)}
+                />
+                {index > 0 && (
+                  <Button variant="danger" onClick={() => handleDeleteField('workingPlaces', index)}>
+                    Delete
+                  </Button>
+                )}
+              </div>
+            </Form.Group>
+          ))}
+          <Button onClick={() => handleAddField('workingPlaces')}>Add Working Place</Button>
+
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="registrationdate">Registration Date</Form.Label>
+            <Form.Control id="registrationdate" type="datetime-local" value={formData.registrationdate} readOnly />
+          </Form.Group>
+
+          <Button type="submit">Register</Button>
+        </fieldset>
+      </Form>
+    </div>
   );
 }
 
